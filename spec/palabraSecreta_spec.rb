076d5ab829a4cb5c8d995572473a73801810b9e4
Yenicei Royero Lopez <yeniceirl@gmail.com>
2018-12-12 18:00:33 -0400

    Corrigiendo rspec pruebas tdd

--- spec/palabraSecreta_spec.rb
+++
@@ -1,8 +0,0 @@
-require './lib/palabraSecreta.rb'
-
-describe PalabraSecreta do
-  it "no permite espacio" do
-    palabraS = PalabraSecreta.new("manzana verde")
-    palabraS.validarPalabra.should == false
-  end
-end
